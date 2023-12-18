85952b6b10136025b170e5d1452acb625cf0567a
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-12-18 14:35:59 +0900

    feat: Performance entity, repository 추가

---
+++ src/main/kotlin/com/performance/domain/performance/model/Performance.kt
@@ -0,0 +1,46 @@
+package com.performance.domain.performance.model
+
+import com.performance.domain.place.model.Place
+import com.performance.global.entity.BaseEntity
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "PERFORMANCE")
+class Performance(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @ManyToOne
+    @JoinColumn(name = "place_id", nullable = false)
+    var place: Place,
+
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Column(name = "description", nullable = false)
+    var description: String,
+
+    @Column(name = "price", nullable = false)
+    var price: BigDecimal,
+
+    @Column(name = "start_at", nullable = false)
+    var startAt: LocalDateTime,
+
+    @Column(name = "end_at", nullable = false)
+    var endAt: LocalDateTime,
+
+    @Column(name = "reservation_start_at", nullable = false)
+    var reservationStartAt: LocalDateTime,
+
+    @Column(name = "reservation_end_at", nullable = false)
+    var reservationEndAt: LocalDateTime,
+
+    @Column(name = "can_reserve", nullable = false)
+    var canReserve: Boolean,
+
+    @Column(name = "available_seat", nullable = false)
+    var availableSeat: Int
+): BaseEntity()
---
+++ src/main/kotlin/com/performance/domain/performance/repository/PerformanceRepository.kt
@@ -0,0 +1,7 @@
+package com.performance.domain.performance.repository
+
+import com.performance.domain.performance.model.Performance
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PerformanceRepository: JpaRepository<Performance, Long> {
+}
