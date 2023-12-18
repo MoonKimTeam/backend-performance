e611f3356f144307f47568c342de6d2b8440f7ed
MALLLAG <87420630+MALLLAG@users.noreply.github.com>
2023-12-18 14:36:05 +0900

    feat: Place entity, repository 추가

---
+++ src/main/kotlin/com/performance/domain/place/model/Place.kt
@@ -0,0 +1,18 @@
+package com.performance.domain.place.model
+
+import com.performance.global.entity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "PLACE")
+class Place(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(name = "name", nullable = false)
+    var name: String,
+
+    @Column(name = "total_seat", nullable = false)
+    var totalSeat: Int
+): BaseEntity()
