e90f497c9831632f3f294716c9e5ba3487b1e582
DonggeonMoon <nullsafer@naver.com>
2023-12-24 22:30:05 +0900

    feat: 공연 기능 작성

--- src/main/kotlin/com/performance/global/exception/ReservationUnavailableException.kt
+++ src/main/kotlin/com/performance/global/exception/ReservationUnavailableException.kt
@@ -1,3 +1,3 @@
 package com.performance.global.exception
 
-class ReservationUnavailableException : IllegalStateException()
+class ReservationUnavailableException(message: String) : IllegalThreadStateException(message)
